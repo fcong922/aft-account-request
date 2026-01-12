e35f3db5951b81a039edc4ac423cf71a7ffab931
fcong922 <fcong922@gmail.com>
2026-01-12 16:12:46 -0500

    Update account-requests.tf 4

--- terraform/account-requests.tf
+++ terraform/account-requests.tf
@@ -44,8 +44,25 @@ module "import-account-1" {
     SSOUserFirstName          = "Admin"
     SSOUserLastName           = "User"
   }
+  account_tags = {
+    "ABC:Owner"       = "myname@mycompany.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
   change_management_parameters = {
     change_requested_by = "AWS Control Tower Lab"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)2"
   }
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
 }
