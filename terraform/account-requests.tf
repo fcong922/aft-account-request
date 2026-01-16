05aa4ddf8614f799f597501f2569ddfb30fe7a52
fcong922 <fcong922@gmail.com>
2026-01-16 12:21:38 -0500

    Update lab 3

--- terraform/account-requests.tf
+++ terraform/account-requests.tf
@@ -122,3 +122,57 @@ module "account_request_02" {
 
   account_customizations_name = "SANDBOX"
 }
+module "account_request_03" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "fcong922+lab3@gmail.com"
+    AccountName               = "Lab Account"
+    ManagedOrganizationalUnit = "test" 
+    SSOUserEmail              = "fcong922+lab3@gmail.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+  account_tags = {
+    "ABC:Owner"       = "myname@mycompany.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)3"
+  }
+  custom_fields = {
+    alternate_contact = jsonencode(
+      {
+        "billing"= {
+          "email-address" = "billing@mycompany.com",
+          "name"          = "Account Receiveable",
+          "phone-number"  = "+11234567890",
+          "title"         = "Billing Department"
+        },
+        "operations"= {
+          "email-address" = "ops@mycompany.com",
+          "name"          = "Operations 24/7",
+          "phone-number"  = "+11234567890",
+          "title"         = "DevOps Team"
+        },
+        "security"= {
+          "email-address" = "soc@mycompany.com",
+          "name"          = "Security Ops Center",
+          "phone-number"  = "+11234567890",
+          "title"         = "SOC Team"
+        }
+      }
+    )
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
