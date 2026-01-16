90b2f8968f74022bf671a9fd7e2f7b538e5adc4e
fcong922 <fcong922@gmail.com>
2026-01-15 22:53:49 -0500

    Create test account

--- terraform/account-requests.tf
+++ terraform/account-requests.tf
@@ -89,3 +89,38 @@ module "import-account-1" {
   account_customizations_name = "SANDBOX"
 }
 }
+module "account_request_02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "fcong922+aftCreation2@gmail.com"
+    AccountName               = "Test Account"
+    ManagedOrganizationalUnit = "Labs" 
+    SSOUserEmail              = "fcong922+aftCreation2@gmail.com"
+    SSOUserFirstName          = "Jane"
+    SSOUserLastName           = "Doe"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "myname@mycompany.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
